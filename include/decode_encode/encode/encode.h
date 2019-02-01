0737bd56f9f1901d83a90829ba6d47472a2b22de
Carlos Enrique Pérez Sánchez <ceperez1996@gmail.com>
2019-02-01 00:53:38 -0500

    Remove the WM and SOS encoding headers, and the database header

--- include/decode_encode/encode/encode.h
+++ include/decode_encode/encode/encode.h
@@ -1,11 +1,6 @@
 #ifndef ENCODE_H
 #define ENCODE_H
 
-#include "enc_wm.h"
-#include "enc_sos.h"
-#include "../dec_enc_common.h"
-#include "../../model/md1global.h"
-
 void bitUnpackingEncoding(char* unpackedIntegers, const char* packedPassword, int bytesToUnpack);
 void lookupTableEncodingSOS(char* password54Chars, const char* password54Integers);
 void realocateBytesEncodingSOS(char* allocatedPassword, const char* unallocatedPassword);
