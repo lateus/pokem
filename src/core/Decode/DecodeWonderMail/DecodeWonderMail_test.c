ab6083069b644902fc8fc447eb540e86efa9056f
Carlos Enrique Pérez Sánchez <ceperez1996@gmail.com>
2019-11-23 22:30:02 -0500

    Write a test for `wonderMailIsInvalid` function (`DecodeWonderMail.c`)

--- src/core/Decode/DecodeWonderMail/DecodeWonderMail_test.c
+++ src/core/Decode/DecodeWonderMail/DecodeWonderMail_test.c
@@ -8,12 +8,14 @@
 CuSuite* DecodeWonderMailGetTestSuite(void);
 
 void decodeWonderMail_test(CuTest *tc);
+void wonderMailIsInvalid_test(CuTest *tc);
 
 
 CuSuite* DecodeWonderMailGetTestSuite()
 {
     CuSuite *suite = CuSuiteNew();
     SUITE_ADD_TEST(suite, decodeWonderMail_test);
+    SUITE_ADD_TEST(suite, wonderMailIsInvalid_test);
     return suite;
 }
 
@@ -29,7 +31,7 @@ void decodeWonderMail_test(CuTest *tc)
     };
     struct WM_INFO input2[ARRAY_SIZE] = {};
 
-    struct WM_INFO expected[ARRAY_SIZE] = {
+    const struct WM_INFO expected[ARRAY_SIZE] = {
         { "Take me!",               "I can't go by myself...",              "Please take me to see Wartortle!",        "Squirtle",  "Escort to Wartortle.", "Thunderwave Cave", "B1F",  'D', "400 poke",                     "1?J9N/X?4P?3\n          4??764?0P??W" },
         { "Take me!",               "Lapras is waiting for me!",            "Please take me to see Lapras!",           "Umbreon",   "Escort to Lapras.",    "Mt. Steel",        " 6F",  'D', "200 poke + ??? [Pecha Scarf]", "4?6F7M+?4JNR\n          J*??K??0+9??" },
         { "Escort me!",             "I have to go!",                        "Someone, please escort me!",              "Combusken", "Escort to Pikachu.",   "Purity Forest",    " 98F", '*', "Friend Zone [Boulder Cave]",   "S62*S40?4P5H\n          8S?869H0!N?W" },
@@ -53,6 +55,33 @@ void decodeWonderMail_test(CuTest *tc)
         CuAssertIntEquals(tc, (int)expected[i].difficulty, (int)input2[i].difficulty);
         CuAssertStrEquals(tc, expected[i].reward, input2[i].reward);
         CuAssertStrEquals(tc, expected[i].WMail, input2[i].WMail);
+    }
+#undef ARRAY_SIZE
+}
+
+void wonderMailIsInvalid_test(CuTest *tc)
+{
+    #define ARRAY_SIZE 9
+    const char input1[ARRAY_SIZE][24 + 1] = {
+        { "1?J9N/X?4P?34??764?0P??W" }, /* (WM - VALID) */
+        { "2?J9N/X?4P?34??764?0P??W" }, /* (WM - INVALID CHECKSUM) */
+        { "4?6F7M+?4JNRJ*??K??0+9??" }, /* (WM - VALID) */
+        { "16?WR3T 48MX13?767?6?6?W" }, /* (WM - INVALID INPUT) */
+        { "S62*S40?4P5H8S?869H0!N?W" }, /* (WM - VALID) */
+        { "???N+CS?466S*+?RX4?5???W" }, /* (WM - VALID) */
+        { "???Y0KS?4PR8**?-6??0?N?W" }, /* (WM - INVALID CHECKSUM) */
+        { "F??CR/0?4/+!*3?7TP?T?7?W" }, /* (WM - VALID) */
+        { "A??CR/0?4/+!*3?7TP?T?7?W" }  /* (WM - INVALID INPUT) */
+    };
+    char input2[ARRAY_SIZE][15] = {0};
+
+    int actual[ARRAY_SIZE];
+    const int expected[ARRAY_SIZE] = { NoError, ChecksumError, NoError, InputError, NoError, NoError, ChecksumError, NoError, InputError };
+
+    int i;
+    for (i = 0; i < ARRAY_SIZE; ++i) {
+        actual[i] = wonderMailIsInvalid(input1[i], input2[i]);
+        CuAssertIntEquals(tc, expected[i], actual[i]);
     }
 #undef ARRAY_SIZE
 }
